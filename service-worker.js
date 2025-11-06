dd5008ff8e9c924a49e0d2b4c0415e02a3bbac16
sako19870518-glitch <sako19870518@gmail.com>
2025-11-06 15:42:55 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,2 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
-self.addEventListener("activate", e => {
-  clients.claim();
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => clients.claim());
